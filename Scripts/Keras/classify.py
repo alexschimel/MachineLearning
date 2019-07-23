2b761c252efda4cd228010b3109ff2af55d9be59
Alex Pletzer <alexander@gokliya.net>
2019-07-24 10:58:01 +1200

    adding dir command line args

--- Scripts/Keras/classify.py
+++ Scripts/Keras/classify.py
@@ -7,7 +7,20 @@ import glob
 import pandas
 import re
 
-numpy.random.seed(13435)
+parser = argparse.ArgumentParser(description='Count the dots.')
+parser.add_argument('--seed', type=int, default=13435, 
+                    help='Random seed')
+parser.add_argument('--trainDir', default='../../Data/Synthetic/Dots/train/',
+                    help='Path to the training data directory')
+parser.add_argument('--testDir', default='../../Data/Synthetic/Dots/test/',
+                    help='Path to the testing data directory')
+
+
+args = parser.parse_args()
+
+
+numpy.random.seed(args.seed)
+
 
 def loadImages(filenames):
 	"""
@@ -38,8 +51,8 @@ def getImageSizes(filename):
 	return im.shape[:2]
 
 
-dataDir = '../../Data/Synthetic/Dots/'
-trainingDir = dataDir + 'train/'
+trainingDir = args.trainDir
+print('train directory: {}'.format(trainingDir))
 
 df = pandas.read_csv(trainingDir + 'train.csv')
 categories = df['numberOfDots'].unique()
@@ -51,8 +64,10 @@ numCategories = maxNumDots - minNumDots + 1
 trainingOutput = (numpy.array(df['numberOfDots'], numpy.float32) - minNumDots)/(maxNumDots - minNumDots)
 trainingInput = loadImages(glob.glob(trainingDir + 'img*.jpg'))
 
-testingDir = dataDir + 'test/'
-df = pandas.read_csv(testingDir + 'test.csv')
+testingDir = args.testDir
+print('test directory: {}'.format(testingDir))
+
+df = pandas.read_csv(testingDir + '/test.csv')
 numCategories = len(categories)
 # labels start at zero
 testingOutput = (numpy.array(df['numberOfDots'], numpy.float32) - minNumDots)/(maxNumDots - minNumDots)
